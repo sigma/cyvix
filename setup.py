a3482a9e82f1db4e80ce4e3fa16795674a022447
Yann Hodique <yann.hodique@gmail.com>
2012-10-13 11:33:40 +0200

    build on OSX

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import sys
+
+library_dirs = None
+
+if sys.platform == 'darwin':
+    library_dirs = ["/Applications/VMware Fusion.app/Contents/Public/"]
 
 ext_modules = [Extension("cyvix", ["cyvix.pyx"],
-                         libraries=["vixAllProducts", "dl", "pthread"])]
+                         libraries=["vixAllProducts", "dl", "pthread"],
+                         library_dirs=library_dirs)]
 
 setup(
     name = 'ViX API',
