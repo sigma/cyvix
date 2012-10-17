ad0a2f2e52202f2da6f7b503d0d2609f5d48aaa0
Yann Hodique <yann.hodique@gmail.com>
2012-10-17 19:59:50 +0200

    add extra link args

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ from Cython.Distutils import build_ext
 import sys
 
 library_dirs = None
+extra_link_args = None
 
 if sys.platform == 'darwin':
     library_dirs = ["/Applications/VMware Fusion.app/Contents/Public/"]
+    extra_link_args = ["-headerpad_max_install_names"]
 
 ext_modules = [Extension("cyvix", ["cyvix.pyx", "vix.pxd"],
                          libraries=["vixAllProducts", "dl", "pthread"],
-                         library_dirs=library_dirs)]
+                         library_dirs=library_dirs,
+                         extra_link_args=extra_link_args)]
 
 setup(
     name = 'ViX API',
