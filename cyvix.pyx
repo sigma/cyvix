32a395a4bc066dbe48b6d00b19cef79d5c895c4f
Yann Hodique <yann.hodique@gmail.com>
2012-10-18 09:28:41 +0200

    add some guest operations to cyvix

--- cyvix.pyx
+++ cyvix.pyx
@@ -42,7 +42,7 @@ cdef class __Host:
         if host is not None:
             self.host = host
         self.port = port
-        self.handle = vix.VIX_HANDLETYPE_NONE
+        self.handle = vix.VIX_INVALID_HANDLE
 
     def connect(self, username, passwd):
         cdef char* _user = NULL
@@ -68,15 +68,15 @@ cdef class __Host:
         self.handle = hostHandle
 
     def disconnect(self):
-        if self.handle is not vix.VIX_HANDLETYPE_NONE:
+        if self.handle is not vix.VIX_INVALID_HANDLE:
             vix.VixHost_Disconnect(self.handle)
-            self.handle = vix.VIX_HANDLETYPE_NONE
+            self.handle = vix.VIX_INVALID_HANDLE
 
     cpdef _findVMs(self, vix.VixFindItemType typ):
         cdef vix.VixHandle jobHandle
         cdef vix.VixError err
         vms = []
-        if self.handle is not vix.VIX_HANDLETYPE_NONE:
+        if self.handle is not vix.VIX_INVALID_HANDLE:
             jobHandle \
                 = vix.VixHost_FindItems(self.handle, typ,
                                         vix.VIX_INVALID_HANDLE, -1,
@@ -134,17 +134,23 @@ cdef class VirtualMachine:
     cdef vix.VixHandle hostHandle
     cdef vix.VixHandle handle
     cpdef public char* path
+    cdef bint loggedin
 
     def __init__(self, char* path, vix.VixHandle hostHandle):
         self.path = path
         self.hostHandle = hostHandle
         self.handle = vix.VIX_INVALID_HANDLE
+        self.loggedin = False
 
     def __dealloc__(self):
         if self.handle != vix.VIX_INVALID_HANDLE:
             vix.Vix_ReleaseHandle(self.handle)
+            self.handle = vix.VIX_INVALID_HANDLE
 
     cpdef open(self):
+        if self.handle != vix.VIX_INVALID_HANDLE:
+            return
+
         cdef vix.VixHandle handle
         cdef vix.VixHandle jobHandle \
             = vix.VixHost_OpenVM(self.hostHandle, self.path,
@@ -156,5 +162,68 @@ cdef class VirtualMachine:
                               vix.VIX_PROPERTY_JOB_RESULT_HANDLE,
                               &handle,
                               vix.VIX_PROPERTY_NONE)
+        vix.Vix_ReleaseHandle(jobHandle)
         VIX_CHECK_ERR_CODE(err)
         self.handle = handle
+
+    cpdef login(self, char* user, char* pwd, interactive=True):
+        self.open()
+        options = vix.VIX_LOGIN_IN_GUEST_REQUIRE_INTERACTIVE_ENVIRONMENT if interactive else 0
+        cdef vix.VixHandle jobHandle \
+            = vix.VixVM_LoginInGuest(self.handle, user, pwd,
+                                     options, NULL, NULL)
+        cdef vix.VixError err \
+            = vix.VixJob_Wait(jobHandle, vix.VIX_PROPERTY_NONE)
+        vix.Vix_ReleaseHandle(jobHandle)
+        VIX_CHECK_ERR_CODE(err)
+        self.loggedin = True
+
+    cpdef logout(self):
+        if not self.loggedin:
+            return
+        cdef vix.VixHandle jobHandle \
+            = vix.VixVM_LogoutFromGuest(self.handle, NULL, NULL)
+        cdef vix.VixError err \
+            = vix.VixJob_Wait(jobHandle, vix.VIX_PROPERTY_NONE)
+        vix.Vix_ReleaseHandle(jobHandle)
+        VIX_CHECK_ERR_CODE(err)
+        self.loggedin = False
+
+    cpdef putFile(self, char* orig, char* dest):
+        cdef vix.VixHandle jobHandle \
+            = vix.VixVM_CopyFileFromHostToGuest(self.handle, orig, dest, 0,
+                                                vix.VIX_INVALID_HANDLE,
+                                                NULL, NULL)
+        cdef vix.VixError err \
+            = vix.VixJob_Wait(jobHandle, vix.VIX_PROPERTY_NONE)
+        vix.Vix_ReleaseHandle(jobHandle)
+        VIX_CHECK_ERR_CODE(err)
+
+    cpdef getFile(self, char* orig, char* dest):
+        cdef vix.VixHandle jobHandle \
+            = vix.VixVM_CopyFileFromGuestToHost(self.handle, orig, dest, 0,
+                                                vix.VIX_INVALID_HANDLE,
+                                                NULL, NULL)
+        cdef vix.VixError err \
+            = vix.VixJob_Wait(jobHandle, vix.VIX_PROPERTY_NONE)
+        vix.Vix_ReleaseHandle(jobHandle)
+        VIX_CHECK_ERR_CODE(err)
+
+    cpdef int runProgram(self, char* prog, char* options, bint block=True):
+        cdef int opts = <int>vix.VIX_RUNPROGRAM_ACTIVATE_WINDOW
+        cdef int err_code
+        if not block:
+            opts |= vix.VIX_RUNPROGRAM_RETURN_IMMEDIATELY
+        cdef vix.VixHandle jobHandle \
+            = vix.VixVM_RunProgramInGuest(self.handle, prog, options,
+                                          <vix.VixRunProgramOptions>opts,
+                                          vix.VIX_INVALID_HANDLE,
+                                          NULL, NULL)
+        cdef vix.VixError err \
+            = vix.VixJob_Wait(jobHandle,
+                              vix.VIX_PROPERTY_JOB_RESULT_ERROR_CODE,
+                              &err_code,
+                              vix.VIX_PROPERTY_NONE)
+        vix.Vix_ReleaseHandle(jobHandle)
+        VIX_CHECK_ERR_CODE(err)
+        return err_code
