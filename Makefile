f3046c8ed3b5e9771520daf6c4ea5502d10df2e8
Yann Hodique <yann.hodique@gmail.com>
2012-10-24 15:10:32 +0200

    move OSX-specific hack to setup.py
    this allows to use "python setup.py install" and the likes

--- Makefile
+++ Makefile
@@ -1,21 +1,11 @@
 PYTHON=python
 TARGET=cyvix.so
-VMWARE_LIB=libvixAllProducts.so
 .PHONY=install
-
-ifeq ($(shell uname), Darwin)
-  VMWARE_LIB=libvixAllProducts.dylib
-  VMWARE_FUSION_LIB_PATH=/Applications/VMware\ Fusion.app/Contents/Public/
-endif
 
 all: $(TARGET)
 
 $(TARGET): $(wildcard *.pxd) $(wildcard *.pyx)
 	$(PYTHON) setup.py build_ext --inplace
-ifeq ($(shell uname), Darwin)
-	install_name_tool -change $(VMWARE_LIB) @rpath/$(VMWARE_LIB) $(TARGET)
-	install_name_tool -add_rpath $(VMWARE_FUSION_LIB_PATH) $(TARGET)
-endif
 
 clean:
 	rm -f $(TARGET) $(subst .pyx,.c,$(wildcard *.pyx)) $(subst .pyx,.h,$(wildcard *.pyx))
--- setup.py
+++ setup.py
@@ -1,19 +1,43 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from distutils.unixccompiler import UnixCCompiler
+from Cython.Distutils import build_ext as _build_ext
+
 import sys
+import os
+
+VMWARE_LIB="libvixAllProducts.dylib"
+VMWARE_FUSION_LIB_PATH="/Applications/VMware\ Fusion.app/Contents/Public/"
 
 library_dirs = None
 extra_link_args = None
 
-if sys.platform == 'darwin':
-    library_dirs = ["/Applications/VMware Fusion.app/Contents/Public/"]
+def onOSX():
+    return sys.platform == 'darwin'
+
+if onOSX():
+    library_dirs = [VMWARE_FUSION_LIB_PATH]
     extra_link_args = ["-headerpad_max_install_names"]
 
-ext_modules = [Extension("cyvix", ["cyvix.pyx", "vix.pxd"],
-                         libraries=["vixAllProducts", "dl", "pthread"],
-                         library_dirs=library_dirs,
-                         extra_link_args=extra_link_args)]
+class VMwareExtension(Extension):
+    pass
+
+class build_ext(_build_ext):
+
+    def build_extension(self, ext):
+        _build_ext.build_extension(self, ext)
+        if isinstance(ext, VMwareExtension):
+            if onOSX():
+                lib_name = ext.name + UnixCCompiler.shared_lib_extension
+                os.system("install_name_tool -change %s @rpath/%s %s"
+                          % (VMWARE_LIB, VMWARE_LIB, lib_name))
+                os.system("install_name_tool -add_rpath %s %s"
+                          % (VMWARE_FUSION_LIB_PATH, lib_name))
+
+ext_modules = [VMwareExtension("cyvix", ["cyvix.pyx", "vix.pxd"],
+                               libraries=["vixAllProducts", "dl", "pthread"],
+                               library_dirs=library_dirs,
+                               extra_link_args=extra_link_args)]
 
 setup(
     name = 'ViX API',
