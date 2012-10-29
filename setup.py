e2c36486d6bc0c8f3852911f23ff98f938c416fa
Yann Hodique <yann.hodique@gmail.com>
2012-10-29 15:51:13 +0100

    build on windows

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ import os
 
 VMWARE_LIB="libvixAllProducts.dylib"
 VMWARE_FUSION_LIB_PATH="/Applications/VMware\ Fusion.app/Contents/Public/"
+VMWARE_VIX_LIB_PATH="C:\\Program Files (x86)\\VMware\\VMware VIX"
 
 library_dirs = None
 extra_link_args = None
@@ -15,9 +16,14 @@ extra_link_args = None
 def onOSX():
     return sys.platform == 'darwin'
 
+def onWin():
+    return sys.platform == 'win32'
+
 if onOSX():
     library_dirs = [VMWARE_FUSION_LIB_PATH]
     extra_link_args = ["-headerpad_max_install_names"]
+elif onWin():
+    library_dirs = [VMWARE_VIX_LIB_PATH]
 
 class VMwareExtension(Extension):
     pass
@@ -34,8 +40,12 @@ class build_ext(_build_ext):
                 os.system("install_name_tool -add_rpath %s %s"
                           % (VMWARE_FUSION_LIB_PATH, lib_name))
 
+if not onWin():
+    libraries = ["vixAllProducts", "dl", "pthread"]
+else:
+    libraries = ["VixAllProductsDyn"]
 ext_modules = [VMwareExtension("cyvix", ["cyvix.pyx", "vix.pxd"],
-                               libraries=["vixAllProducts", "dl", "pthread"],
+                               libraries=libraries,
                                library_dirs=library_dirs,
                                extra_link_args=extra_link_args)]
 
